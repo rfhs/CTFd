813bb69d41864710b51c7c08d172a18a1a20a3dd
Gobo42 <hum.matt@gmail.com>
2023-01-21 18:46:31 -0500

    Initial mods

---
+++ CTFHandler/handler.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Very simple HTTP server in python for logging requests
+Usage::
+    ./server.py [<port>]
+"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(302)
+        self.send_header('Location', '/user')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+ #      logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",str(self.path), str(self.headers), post_data.decode('utf-8'))
+        vals=post_data.decode('utf-8').split("&")
+        print(vals)
+        uid=vals[0].split("=")
+        action=vals[1].split("=")
+        print(uid,action)
+        if action[1]=="startvm":
+            print("Start VM for user "+uid[1])
+        elif action[1]=="restartvm":
+            print("Restart VM for user "+uid[1])
+        else:
+            print("Stopping VM for user "+uid[1])
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    run(port=4001)
